5907b1e0bde90d53694ded559bfed22c011439e0
Sandeep  A Nair <snair3@atlassian.com>
2026-03-31 17:17:40 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let solution = [];
+        const remainder = {};
+        for(let i = 0; i<nums.length;i++) {
+            const diff = target - nums[i];
+            if(diff in remainder) {
+                solution.push(remainder[diff]);
+                solution.push(i);
+                break;
+            } else {
+                remainder[nums[i]] = i;
+            }
+        }
+        return solution
+    }
+}
