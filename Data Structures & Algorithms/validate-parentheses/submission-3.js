f0168084f1b36a6aa648c731dd31c06ee6ad1c9b
Sandeep  A Nair <snair3@atlassian.com>
2026-04-01 18:44:35 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+
+        const map = {
+            "(":")",
+            "{":"}",
+            "[":"]"
+        }
+        let i=0;
+        if(s.length === 1) {
+            return false;
+        }
+        for(; i<s.length;i++) {
+            if(s[i]=="(" || s[i]=="{"  || s[i]=="[" ) {
+                stack.push(s[i]);
+            }
+            if(s[i]==")" || s[i]=="}"  || s[i]=="]" ) {
+                const lastParen = stack[stack.length-1];              
+                if(s[i] === map[lastParen]) {
+                    stack.pop()
+                } else {
+                    break;
+                }
+            }
+        }
+        return stack.length === 0 && !(i<s.length);
+    }
+}
