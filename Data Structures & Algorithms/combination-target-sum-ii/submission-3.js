0a2cf644cefc961d82c9a558a46ed96daa602e6d
Sandeep  A Nair <snair3@atlassian.com>
2026-04-08 20:05:04 +0530

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        const subsets = [];
+        const sortedCand = candidates.sort((a,b) => a-b)
+        const findCand = (subs, remaining, currSub, i) => {
+            if(remaining === 0) {
+                subs.push([...currSub]);
+                return;
+            }
+
+            if(i>=sortedCand.length || remaining<0) {
+                return;
+            }
+            currSub.push(sortedCand[i]);
+            findCand(subs, remaining - sortedCand[i], currSub, i+1);
+            currSub.pop();
+            while(i+1<sortedCand.length && sortedCand[i] === sortedCand[i+1]) {
+                i++;
+            }
+            findCand(subs, remaining, currSub, i+1);
+        }
+        findCand(subsets, target,[],0)
+        return subsets;
+    }
+}
