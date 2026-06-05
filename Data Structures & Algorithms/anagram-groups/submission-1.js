4de5f1ba5daa84d142c4b634f877b41d406ff6c9
Sandeep  A Nair <snair3@atlassian.com>
2026-06-05 17:44:14 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const anagramList = {};
+        for(let i = 0; i< strs.length; i++) {
+            const letterCount = Array(26).fill(0)
+            const currWord = strs[i]
+
+            currWord.split("").reduce((acc, letter) => {
+                const index = letter.charCodeAt(0) - 'a'.charCodeAt(0);
+                acc[index]+=1;
+                return acc;
+            },letterCount)
+            letterCount.join("")
+            if(letterCount in anagramList) {
+                anagramList[letterCount].push(currWord)
+            } else {
+                anagramList[letterCount] = [currWord];
+            }
+        }
+        return Object.values(anagramList)
+    }
+}
