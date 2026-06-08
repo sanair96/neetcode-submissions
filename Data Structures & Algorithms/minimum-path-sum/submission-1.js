3b028f0f606bbfd9eb25ff4545989938df5ee0d3
Sandeep  A Nair <snair3@atlassian.com>
2026-06-08 15:48:54 +0530

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+
+    minPathSum(grid) {
+       const ROWS = grid.length;
+       const COLS = grid[0].length;
+
+       const dp = Array.from({length: ROWS+1}, () => new Array(COLS+1).fill(Infinity));
+        
+        dp[ROWS-1][COLS] = 0;
+        for(let i = ROWS-1;i>=0;i--) {
+            for(let j = COLS-1;j>=0;j--) {
+                dp[i][j] = grid[i][j]+Math.min(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0]
+    }
+}
