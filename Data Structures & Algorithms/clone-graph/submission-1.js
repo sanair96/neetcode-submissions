cbd14fdef334f186d69cd183567b4e3710ff3307
Sandeep  A Nair <snair3@atlassian.com>
2026-06-08 15:50:19 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const visited = {};
+        const clone = (copyNode, vis) => {
+            if(copyNode === null) {
+                return null;
+            }
+
+            if(vis[copyNode.val]) {
+                return vis[copyNode.val];
+            }
+            if(copyNode.neighbors.length === 0) {
+                const newNode = new Node(copyNode.val, [])
+                vis[copyNode.val] = newNode;
+                return newNode;
+            }
+
+            const newNode = new Node(copyNode.val);
+            vis[copyNode.val] = newNode;
+            newNode.neighbors = copyNode.neighbors.map(adjNode => clone(adjNode, vis))
+            return newNode;
+        }
+        return clone(node, visited);
+    }
+}
